d0eb39215f0f68a98874ec83a1989a81ca97e701
Shalennyy <shalennyyv@gmail.com>
2019-05-28 11:03:42 +0300

    first step

---
+++ __init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'thisismysecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from .auth import auth as auth_bl
+    app.register_blueprint(auth_bl)
+
+    from .main import main as main_bl
+    app.register_blueprint(main_bl)
+
+    return app
+
+# def create_app(config_file_name, chdir):
+#     app = Flask(__name__)
+#     return app
